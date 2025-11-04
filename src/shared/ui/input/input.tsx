312d26bc1bcbaa56b1468e1c4c60c8e1b1c0b710
Petro Sahal <petrosahal66@gmail.com>
2025-11-04 19:10:07 +0200

    feat: add input

---
+++ src/shared/ui/input/input.tsx
@@ -0,0 +1,27 @@
+import {type ChangeEvent, type ComponentPropsWithRef} from 'react'
+import clsx from 'clsx'
+import search from '@/shared/assets/icons/search.svg'
+
+import s from './input.module.scss'
+
+type Props = {
+    value?: string
+    fullWidth?: boolean
+    onChange?: (event: ChangeEvent<HTMLInputElement>) => void
+} & ComponentPropsWithRef<'input'>
+
+export const Input = ({ value, onChange, fullWidth, className, ref }: Props) => {
+    return (
+        <div className={clsx(s.container, className && className)}>
+            <img className={clsx(s.searchIcon)} src={search} alt="пошук" />
+            <input
+                ref={ref}
+                onChange={onChange}
+                value={value}
+                className={clsx(s.input, fullWidth && s.fullWidth)}
+                placeholder={'Пошук'}
+            />
+            <div className={s.inputTextBlock}>Пошук</div>
+        </div>
+    )
+}
