4f6d6c02f3ca4d684d0a64cfd8e86dedb37cea0c
Petro Sahal <petrosahal66@gmail.com>
2025-11-04 21:44:44 +0200

    feat: connect store and RTK Query

---
+++ src/shared/api/baseApi.ts
@@ -0,0 +1,18 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+
+export const baseApi = createApi({
+    reducerPath: "baseApi",
+    baseQuery: async (args, api, extraOptions) => {
+        const result = await fetchBaseQuery({
+            baseUrl: import.meta.env.VITE_BASE_URL,
+            prepareHeaders: (headers) => {
+                headers.set("API-KEY", import.meta.env.VITE_API_KEY)
+            },
+        })(args, api, extraOptions)
+
+        console.error(api, result)
+
+        return result
+    },
+    endpoints: () => ({}),
+})
