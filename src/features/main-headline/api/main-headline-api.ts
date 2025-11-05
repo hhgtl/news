0dc23febfd6ba6818b87cb42314539891e610cd2
Petro Sahal <petrosahal66@gmail.com>
2025-11-05 18:15:59 +0200

    feat: deploy to github pages and change env

--- src/features/main-headline/api/main-headline-api.ts
+++ src/features/main-headline/api/main-headline-api.ts
@@ -5,7 +5,7 @@ export const mainHeadlineApi = baseApi.injectEndpoints({
     endpoints: (build) => ({
         getMainHeadline: build.query<HeadlineResponse, { category: string, searchPhrase: string }>({
             query: ({category, searchPhrase}) => {
-                return `/top-headlines?country=us&category=${category}&q=${searchPhrase}&apiKey=7aaf407c36c34091bd531bb0137eeb3f`
+                return `/top-headlines?country=us&category=${category}&q=${searchPhrase}&apiKey=${import.meta.env.VITE_API_KEY}`
             },
         }),
     }),
